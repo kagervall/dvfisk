9157066d79ace35702ca1ead43de0219b57278da
Anders Kagervall <anders.kagervall@gmail.com>
2025-03-04 23:55:01 +0100

    Update SITES.R

--- data-raw/SITES.R
+++ data-raw/SITES.R
@@ -2,8 +2,8 @@ utils::download.file(
   "https://www.slu.se/globalassets/ew/org/inst/aqua/externwebb/databaser/elprovfiskedatabasen/lista-elfiskelokaler-svenskt-elfiskeregister-220503.xlsx",
   destfile = "data-raw/sites.xlsx")
 sites <- readxl::read_excel("data-raw/sites.xlsx", sheet = "Lokaluppgifter Elfiskelokaler")
-sites_sf <- sf::st_as_sf(sites, coords = c("DDLONG","DDLAT"), crs = 4326)
-
+sites <- sf::st_as_sf(sites, coords = c("DDLONG","DDLAT"), crs = 4326)
+usethis::use_data(sites, overwrite = TRUE)
 sites_documentation <- readxl::read_excel("data-raw/sites.xlsx",
                                           sheet = "Information",
                                           skip = 5)

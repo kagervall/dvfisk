eccd253da355b30c835e6f5e2c8fd0c55d4eb32e
Anders Kagervall <anders.kagervall@gmail.com>
2025-03-04 16:31:41 +0100

    First version of API

--- R/API.R
+++ R/API.R
@@ -1,27 +1,149 @@
-library(httr2)
-library(jsonlite)
+#' Internal function to get the base request for the SERS API
+#'
+#' @param one of "lan", "kommuner", "huvudavrinningsomraden", "rapport", the defined
+#' endpoints in the SersVix API
+#'
+#' @returns
+#' A httr2 request object
+#'
+#' @examples
+#' \dontrun{
+#' req <- .sers_data_endpoint("lan")
+#' }
+.sers_data_endpoint <-function(what) {
+  allowed <- c("lan", "kommuner", "huvudavrinningsomraden", "rapport")
+  if (!what %in% allowed) {
+    stop("Internal error in package sersapi'what' parameter. Must be one of: ", paste(allowed, collapse = ", "))
+  }
+  base_url <- sersapi_options()$base_url
+  url <- paste0(base_url, "/", what)
+  req <- httr2::request(url)
+  return(req)
+}
 
-# Get the data
-sers_data_vix_rapport <- function(
-    haroNr, startyear, endyear,
-    cols = c("harO_NR", "huvudavrinningsområde", "vattendrag", "fiskedatum", "lokal",
-             "xkoorlok", "ykoorlok",  "wgS84_Dec_N", "wgS84_Dec_E", "lax0", "lax",
-             "öring0", "öring", "hoh", "syfte")) {
-  url <- "https://dvfisk.slu.se/api/V1/sers/data-vix/rapport"
-  
-  req <- request(url) |>
-    req_url_query(haroNr = haroNr,
-                  startdatum = sprintf("%s-01-01", startyear),
-                  slutdatum = sprintf("%s-12-31", endyear))
+#' Return counties (län) known in the SERS database
+#'
+#' @returns
+#' A vector of type character containing names of counties (län) known in the SERS database
+#' @export
+#'
+#' @seealso
+#' \url{https://dvfisk.slu.se/swagger/index.html}
+#'
+#' @examples
+#' # This example will access the Internet to get the data and will not work offline
+#' counties <- sers_data_vix_lan()
+#' counties
+sers_data_vix_lan <- function() {
+  req <- .sers_data_endpoint("lan")
   resp <- httr2::req_perform(req)
-  body <- httr2::resp_body_json(resp, simplifyVector = TRUE) |>
-    dplyr::select(all_of(cols))
+  body <- httr2::resp_body_json(resp, simplifyVector = TRUE)
+  return(body)
+}
+
+#' Get the municipalities (kommuner) in a county (län)
+#'
+#' This function returns the municipalities (kommuner) in a county (län) in Sweden.
+#' The parameter `lan` must be a valid county (län) in Sweden.
+#'
+#' @param lan A character string with the name of a county (län) in Sweden
+#'
+#'
+#' @returns
+#' A vector of type character containing names of municipalities (kommuner) in the county (län)
+#' @export
+#'
+#' @examples
+#' # This example will access the Internet to get the data and will not work offline
+#' kommuner <- sers_data_vix_kommuner("Västra Götalands län")
+#' kommuner
+sers_data_vix_kommuner <- function(lan) {
+  req <- .sers_data_endpoint("kommuner") |>
+    httr2::req_url_query(lan = lan)
+  resp <- httr2::req_perform(req)
+  body <- httr2::resp_body_json(resp, simplifyVector = TRUE)
+  return(body)
+}
+
+#' Get the main drainage areas (huvudavrinningsområden) in Sweden known in the SERS database
+#'
+#' This function returns the main drainage areas (huvudavrinningsområden) in Sweden.
+#'
+#' @returns
+#' A data frame with columns `haroNr` and `huvudavrinningsomrade` with the main
+#' drainage areas (huvudavrinningsområden) in Sweden
+#' @export
+#'
+#' @examples
+#' # This example will access the Internet to get the data and will not work offline
+#' huvudavrinningsomraden <- sers_data_vix_huvudavrinningsomraden()
+#' head(huvudavrinningsomraden)
+#'
+sers_data_vix_huvudavrinningsomraden <- function() {
+  req <- .sers_data_endpoint("huvudavrinningsomraden")
+  resp <- httr2::req_perform(req)
+  body <- httr2::resp_body_json(resp, simplifyVector = TRUE)
   return(body)
 }
 
 
-req <- httr2::request(url) |>
-  httr2::req_url_query(haroNr = 24000, startdatum = "2021-01-01", slutdatum = "2021-12-31")
+#' Get electrofishing data from the SERS API
+#'
+#' This function returns electrofishing data from the SERS API. You can filter the data by
+#' county (lan), municipality (kommun), main drainage area (haroNr), start date (startdatum)
+#' and end date (slutdatum). You must provide at least one of theparameters. The function
+#' does not check if the parameters are valid and it is possible to give combinations
+#' that does not make sense, e.g. a `haroNr` and `lan` that does not intersect.
+#' It is also possible send requests that return a lot of data, so be careful.
+#'
+#' @param lan character string with the name of a county (län) in Sweden
+#' @param kommun character string with the name of a municipality (kommun) in Sweden
+#' @param haroNr integer with the main drainage area (huvudavrinningsområde) number
+#' @param startdatum character string with the start date in the format "YYYY-MM-DD"
+#' @param slutdatum character string with the end date in the format "YYYY-MM-DD"
+#'
+#' @returns
+#' A data frame with the electrofishing data. There are 244 columns in the data frame
+#' consult SERS-documentation.
+#' @export
+#'
+#' @examples
+#' # This example will access the Internet to get the data and will not work offline
+#' # Get all data from the main drainage area 86000 (Mörrumsån) in 2021
+#' data <- sers_data_vix_rapport(haroNr = 86000, startdatum = "2021-01-01", slutdatum = "2021-12-31")
+#' head(data)
+#' dim(data)
+sers_data_vix_rapport <- function(lan = NULL, kommun = NULL, haroNr = NULL,
+                                  startdatum = NULL, slutdatum = NULL) {
+  req <- .sers_data_endpoint("rapport")
+  if (!is.null(lan) & !is.null(kommun) & !is.null(haroNr) &
+      !is.null(startdatum) & !is.null(slutdatum)) {
+    stop("You must provide at least one of the following parameters: lan, kommun, haroNr, startdatum, slutdatum")
+  }
 
+  if (!is.null(lan)) {
+    req <- req |>
+      httr2::req_url_query(lan = lan)
+  }
+  if (!is.null(kommun)) {
+    req <- req |>
+      httr2::req_url_query(kommun = kommun)
+  }
+  if (!is.null(haroNr)) {
+    req <- req |>
+      httr2::req_url_query(haroNr = haroNr)
+  }
+  if (!is.null(startdatum)) {
+    req <- req |>
+      httr2::req_url_query(startdatum = startdatum)
+  }
+  if (!is.null(slutdatum)) {
+    req <- req |>
+      httr2::req_url_query(slutdatum = slutdatum)
+  }
 
-body <- httr2::resp_body_json(resp, simplifyVector = TRUE)
+  resp <- httr2::req_perform(req)
+  body <- httr2::resp_body_json(resp, simplifyVector = TRUE)
+  return(body)
+}
+

4bfaec09578d16cf4de4f9bdb8c35b6433e6e405
Anders Kagervall <anders.kagervall@gmail.com>
2025-03-05 23:01:24 +0100

    Fixes for name change to dvfisk

--- R/OPTIONS.R
+++ R/OPTIONS.R
@@ -5,32 +5,32 @@
 # Variable, global to package's namespace.
 # This function is not exported to user space and does not need to be documented.
 OPTIONS <- settings::options_manager(
-  base_url = "https://dvfisk.slu.se/api/V1/sers/data-vix")
+  SersVix_url = "https://dvfisk.slu.se/api/V1/sers/data-vix")
 
 
 
 # User function that gets exported:
 
-#' Set or get options for the `sersapi` package
+#' Set or get options for the `dvfisk` package
 #'
 #' @param ... Option names to retrieve option values or \code{[key]=[value]} pairs to set options.
 #'
 #' @section Supported options:
 #' Supported keys:
 #' \itemize{
-#'  \item{\code{base_url} Base URL for the API (Default: "https://dvfisk.slu.se/api/V1/sers/data-vix")}
+#'  \item{\code{SersVix_url} Base URL for the API (Default: "https://dvfisk.slu.se/api/V1/sers/data-vix")}
 #' }
 #'
 #' @examples
 #' # Show default
-#' sersapi_options()
+#' dvfisk_options()
 #'
-#' sersapi_options(base_url = "https://testing.slu.se/api/V1/sers/data-vix") # Change another server
-#' sersapi_options()$base_url # Show the new value
-#' settings::reset(sersapi_options) # Reset to defaults
+#' dvfisk_options(SersVix_url = "https://testing.slu.se/api/V1/sers/data-vix") # Change another server
+#' dvfisk_options()$SersVix_url # Show the new value
+#' settings::reset(dvfisk_options) # Reset to defaults
 #'
 #' @export
-sersapi_options <- function(...){
+dvfisk_options <- function(...){
   # protect against the use of reserved words.
   settings::stop_if_reserved(...)
   OPTIONS(...)

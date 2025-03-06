df3e9fe309b5c576e56df76fe99135c37af12295
Anders Kagervall <anders.kagervall@gmail.com>
2025-03-06 16:04:32 +0100

    Add dvfisk-package.R

---
+++ R/dvfisk-package.R
@@ -0,0 +1,9 @@
+#' @description
+#' To learn more about `dvfisk`, start with the vignettes:
+#' `browseVignettes(package = "dvfisk")`
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL

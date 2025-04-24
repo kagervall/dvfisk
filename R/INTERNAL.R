4101f3acc3445277f55f7a7cca80813b3faf1aa9
Anders Kagervall <anders.kagervall@gmail.com>
2025-04-24 16:51:40 +0200

    Adding API KulFangst

--- R/INTERNAL.R
+++ R/INTERNAL.R
@@ -17,7 +17,7 @@
 #' req <- .dvfiskendpoint("SersVix", "lan")
 #' }
 .dvfisk_endpoint <-function(what, endpoint) {
-  known_apis <- c("SersVix", "SersAggregerad")
+  known_apis <- c("KulFangst", "SersVix", "SersAggregerad")
   if (!what %in% known_apis) {
     stop("Internal error in package dvfisk 'what' parameter. Must be one of: ", paste(known_apis, collapse = ", "))
   }
